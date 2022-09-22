1929e5267996e0131ea9a11ccf40a8d1ddc5e23a
BurhanuddinRaja <mails4burhan@gmail.com>
2022-09-22 09:27:58 +0530

    new app created

---
+++ simple-node-app/index.js
@@ -0,0 +1,11 @@
+const express = require('express')
+
+let app = express()
+
+app.get("/", (req, res) => {
+    res.send("<h2>Hello World!!</h2>")
+})
+
+const port = process.env.PORT || 3000
+
+app.listen(port, () => console.log("Listening to http://localhost:3000 "))
